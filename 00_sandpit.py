9f8f8a0d98cf31740846ef6a89efa4887849b08c
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-12-06 21:15:23 +1300

    Completed day 6

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,13 +1,37 @@
-content_list = [
-    "abc", "abc", "abac", "aaaa", "b"
-]
+data_list = [
+    ['kimczeyaqwbs', 'pwmsf', 'wgmfus', 'lofjwnms', 'rwsum'],
+['cxns', 'c', 'cbv', 'c']
+    ]
 
-sum = 0
+grand_total = 0
 
-for item in content_list:
-    unique = set(item)
-    print(unique)
-    sum += len(unique)
+for item in data_list:
+    people = len(item)
+    common = 0
+    total = 0
 
-print(sum)
+    # Generate entire string
+    item_string = ""
+    for word in item:
+        item_string += word
+    print("Item string", item_string)
+    print()
+
+    # get letters of first item in list
+    for letter in item[0]:
+        print(letter)
+        letter_count = item_string.count(letter)
+        print("letter count: ", letter_count)
+
+        if letter_count == people:
+            total += 1
+
+    grand_total += total
+
+    print("total", total)
+
+    print()
+
+print("Grand Total: ", grand_total)
+
 
